4e173a95eeecb0e2e0282d8780875f86b66f2412
dtmajors <darkfail83@gmail.com>
2026-09-02 22:07:41 -0400

    Bind web server for Railway networking

--- server.js
+++ server.js
@@ -466,7 +466,7 @@ app.use((error, req, res, next) => {
 
 async function start() {
   await db.initializeDatabase();
-  httpServer = app.listen(config.port, () => console.log(`Mortal Nexus website running at ${config.baseUrl}`));
+  httpServer = app.listen(config.port, '0.0.0.0', () => console.log(`Mortal Nexus website running at ${config.baseUrl}`));
 }
 
 start().catch((error) => {
